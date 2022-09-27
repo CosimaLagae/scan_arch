636df81800818efe8c6b04c30ac9d91be0024834
CosimaLagae <cosima.lagae@student.ehb.be>
2022-09-27 14:44:06 +0200

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,42 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
-const IndexPage = () => {
+import { graphql } from 'gatsby'
+
+const IndexPage =  ({ data }) => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      
-      <StaticImage
+    <Layout pageTitle="Home Page"> 
+          <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+     
+{/*       <StaticImage
         alt="archive pitures of graphic design"
         src="../images/arch/Scan_202110195.jpg"
-      />
+        className='img'
+      /> */}
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="Home Page" />
 
 export default IndexPage
